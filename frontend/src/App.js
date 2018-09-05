63d43c9b8d7f690e07265118b1b0c30abe721e46
Kadi Rosenthal <Kadi.Rosenthal@gmail.com>
2018-09-05 18:02:49 +0300

    Copying...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,58 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Kohtumised käeulatuses!</h1>
-        </header>
-          <h1> SIIA TULEB PEALKIRI </h1>
           <body>
-          <form name="form">
-              <div className="form group">
-                  <label htmlFor="username">Username</label>
-                  <input type="text" className="form-control" name="username" />
+          <div className="limiter">
+              <div className="wrap-login100">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Kohtumised käeulatuses!</h1>
+              <div className="container-login100">
+                  <Form>
+                      <FormGroup row>
+                          <Label for="username" sm={3}>Kasutaja:</Label>
+                          <Col sm={8}>
+                            <Input type="text" name="username" id="username" placeholder="Sisestage kasutajanimi" />
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Label for="Password" sm={3}>Parool:</Label>
+                          <Col sm={8}>
+                          <Input type="password" name="password" id="Password" placeholder="Sisestage parool" />
+                          </Col>
+                      </FormGroup>
+                      <Button className="login-btn">LOGI SISSE</Button> <Button className="new-user-button">Loo uus kasutaja</Button>
+
+                          <div className="text-right p-t-13 p-b-23">
+                              <p className="txt1">Unustasid <a href="#" className="txt2">kasutajanime/parooli?</a></p>
+                          </div>
+                  </Form>
+                  </div>
               </div>
-              <div className="form error">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" className="form-control" name="password"/>
-              </div>
-              <div className="form-group">
-                  <button className="btn btn-primary">Login</button>
-                  <button className="btn btn-link">Register</button>
-              </div>
-          </form>
+          </div>
           </body>
       </div>
+
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+//import AppPage from './AppPage';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
