fd3583391aac3aff31f6a30967f638804cdfebca
Triin <triin.lakspere@ttu.ee>
2018-09-06 10:57:25 +0300

    Enhanced login window

--- Vali-IT-projekt-frontend/src/App.js
+++ Vali-IT-projekt-frontend/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
 import {Button, Form, FormGroup, Label, Input, Col} from 'reactstrap';
 import logo from './logo.svg';
 import './css/App.css';
-import Modal from 'react-modal';
+//import Modal from 'react-modal';
+import {Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class App extends Component {
-    constructor(props,context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
 
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
+        this.toggle = this.toggle.bind(this);
 
         this.state = {
-            show: false
+            modal: false
         };
     }
 
-    handleClose() {
-        this.setState({ show: false });
-    }
-
-    handleShow() {
-        this.setState({ show: true });
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
     }
 
   render() {
@@ -35,18 +32,16 @@ class App extends Component {
               <div className="container-login100">
                   <Form>
                       <FormGroup row>
-                          <Label for="username" sm={3}>Kasutaja:</Label>
-                          <Col sm={8}>
-                            <Input type="text" name="username" id="username" placeholder="Sisestage kasutajanimi" />
+                          <Col sm={11}>
+                            <Input type="text" name="username" id="username" placeholder="   Kasutajanimi" />
                           </Col>
                       </FormGroup>
                       <FormGroup row>
-                          <Label for="Password" sm={3}>Parool:</Label>
-                          <Col sm={8}>
-                          <Input type="password" name="password" id="Password" placeholder="Sisestage parool" />
+                          <Col sm={11}>
+                          <Input type="password" name="password" id="password" placeholder="   Parool" />
                           </Col>
                       </FormGroup>
-                      <Button className="login-btn">LOGI SISSE</Button> <Button className="new-user-button" onClick={this.handleShow}>
+                      <Button className="login-btn">LOGI SISSE</Button> <Button className="new-user-button" onClick={()=>this.toggle}>
                         Loo uus kasutaja
                         </Button>
                           <div className="text-right p-t-13 p-b-23">
@@ -56,16 +51,15 @@ class App extends Component {
                   </div>
               </div>
           </div>
-          <Modal show={this.handleShow()} onClose={this.handleClose}>
-              <div>I am a modal</div>
-              <form>
-                  <input />
-                  <button>tab navigation</button>
-                  <button>stays</button>
-                  <button>inside</button>
-                  <button>the modal</button>
-              </form>
-              <button onClick={this.handleClose}>close</button>
+          <Modal isOpen={this.state.show} toggle={this.toggle} className={this.props.className}>
+              <ModalHeader>Modal title</ModalHeader>
+              <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </ModalBody>
+              <ModalFooter>
+                  <Button color="primary">LOO</Button>{' '}
+                  <Button color="secondary">TÜHISTA</Button>
+              </ModalFooter>
           </Modal>
       </div>
     );
