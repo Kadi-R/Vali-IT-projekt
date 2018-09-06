c4b9dff295bc610ea614e084c26036bbd6671c1c
Kadi Rosenthal <Kadi.Rosenthal@gmail.com>
2018-09-06 14:27:23 +0300

    parandatud vaade

--- frontend/src/AppPage.js
+++ frontend/src/AppPage.js
@@ -1,50 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {Component} from 'react'
 import './css/AppPage.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
 
 class AppPage extends Component {
 
-    edit() {};
-    add() {};
-    delete() {};
+    edit() {
+    };
+
+    add() {
+    };
+
+    delete() {
+    };
 
     render() {
-        return (<div>
-                <header>
-                    <nav>
-                        <button type="button">Sign out!</button>
-                    </nav>
-                </header>
-                <div className="AppPage">
-                    <h1> Hello - Kadi</h1>
+        return (
+            <div className = "AppPage">
+                <body>
+                <div className="Wrap">
+                    <header>
+                        <nav>
+                            <button type="button" className="btn btn-secondary" onClick="prepareCustomerAdd()">LOGI
+                                VÄLJA
+                            </button>
+                        </nav>
+                    </header>
+
+                    <div className="Greeting">
+                        <h1> Hello - Triin</h1>
+                    </div>
+                    <div className="wrapper">
+                        <div className="container3">
+                            <table>
+                                <tr>
+                                    <th>Nimi</th>
+                                    <th>Aeg</th>
+                                    <th>Kirjeldus</th>
+                                    <th>Tehtud</th>
+                                    <th>Valikud</th>
+                                </tr>
+                                <tr>
+                                    <td>Arsti aeg</td>
+                                    <td>30. oktoober 2018</td>
+                                    <td>Silmaarsti aeg. Silmaarsti nimi on: dr Kukk</td>
+                                    <td><Input type="checkbox" id="checkbox2"/>{' '}</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Sõbraga kokkusaamine</td>
+                                    <td>11. september kella 12 aeg</td>
+                                    <td>Saan sõbraga kokku kohvikus hommikul</td>
+                                    <td><Input type="checkbox" id="checkbox2"/>{' '}</td>
+                                </tr>
+
+                            </table>
+
+
+                        </div>
+                    </div>
+
+
                 </div>
-                <div className="container3">
-                    <table>
-                        <tr>
-                            <th>Event Name</th>
-                            <th>Time</th>
-                            <th>Description</th>
-                            <th>Done</th>
-                            <th>Options</th>
-                        </tr>
-                        <tr>
-                            <td>Arsti aeg</td>
-                            <td>30. oktoober 2018</td>
-                            <td>Silmaarsti aeg. Silmaarsti nimi on: dr Kukk</td>
-                            <td><Input type="checkbox" id="checkbox2" />{' '}</td>
-                            <td><Button>Muuda</Button><Button>Kustuta</Button></td>
-                        </tr>
-                        <tr>
-                            <td>Sõbraga kokkusaamine</td>
-                            <td>11. september kella 12 aeg</td>
-                            <td>Saan sõbraga kokku kohvikus hommikul</td>
-                            <td><Input type="checkbox" id="checkbox2" />{' '}</td>
-                            <td><Button>Muuda</Button><Button>Kustuta</Button></td>
-                        </tr>
-                    </table>
-                </div>
+
+                </body>
             </div>
-    );
+        )
+            ;
     }
 }
+
 export default AppPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-//import AppPage from './AppPage';
-import App from './App';
+import AppPage from './AppPage';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<AppPage />, document.getElementById('root'));
 registerServiceWorker();
