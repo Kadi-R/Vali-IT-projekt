269ea99d93c12ae62958017437995692e2d6807f
Kadi Rosenthal <Kadi.Rosenthal@gmail.com>
2018-09-07 15:13:58 +0300

    lihtsa modaalakna töötav näide. slelest võiks edasi arendada kuidagi.

--- Vali-IT-projekt-frontend/src/AppPage.js
+++ Vali-IT-projekt-frontend/src/AppPage.js
@@ -1,50 +1,131 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {Component} from 'react'
 import './css/AppPage.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import Modal from 'react-responsive-modal';
+
 
 class AppPage extends Component {
+    state = {
+        open: false,
+    };
 
-    edit() {};
-    add() {};
-    delete() {};
+    onOpenModal = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal = () => {
+        this.setState({open: false});
+    };
 
     render() {
-        return (<div>
-                <header>
-                    <nav>
-                        <button type="button">Sign out!</button>
-                    </nav>
-                </header>
-                <div className="AppPage">
-                    <h1> Hello - Kadi</h1>
+        const {open} = this.state;
+        return (
+            <div className="AppPage">
+                <body>
+                <div className="Wrap">
+                    <header>
+                        <nav>
+                            <button type="button" id="logout" className="btn btn-secondary"
+                                    onClick="prepareCustomerAdd()">LOGI
+                                VÄLJA
+                            </button>
+                        </nav>
+                    </header>
+
+                    <div className="Greeting">
+                        <h1> Hello - Triin</h1>
+                    </div>
+                    <div className="wrapper">
+                        <div className="container3">
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Nimi</th>
+                                    <th>Aeg</th>
+                                    <th>Kirjeldus</th>
+                                    <th>Tehtud</th>
+                                    <th>Valikud</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>Arsti aeg</td>
+                                    <td>30. oktoober 2018</td>
+                                    <td>Silmaarsti aeg. Silmaarsti nimi on: dr Kukk</td>
+                                    <td><Input type="checkbox" id="checkbox2"/>{' '}</td>
+                                    <div className="buttons">
+                                        <button type="button" id="change" className="btn btn-secondary"
+                                                onClick="prepareCustomerAdd()">MUUDA
+
+                                        </button>
+                                        <button type="button" id="delete" className="btn btn-secondary"
+                                                onClick="prepareCustomerAdd()">KUSTUTA
+                                        </button>
+                                    </div>
+
+                                </tr>
+
+                                <tr>
+                                    <td>Sõbraga kokkusaamine</td>
+                                    <td>11. september kella 12 aeg</td>
+                                    <td>Saan sõbraga kokku kohvikus hommikul</td>
+                                    <td><Input type="checkbox" id="checkbox2"/>{' '}</td>
+                                    <div className="buttons">
+                                        <button type="button" id="change" className="btn btn-secondary"
+                                                onClick="prepareCustomerAdd()">MUUDA
+                                        </button>
+                                        <button type="button" id="delete" className="btn btn-secondary"
+                                                onClick="prepareCustomerAdd()">KUSTUTA
+                                        </button>
+                                    </div>
+                                </tr>
+                                </tbody>
+
+                            </table>
+
+
+                        </div>
+                    </div>
+
+
                 </div>
-                <div className="container3">
-                    <table>
-                        <tr>
-                            <th>Event Name</th>
-                            <th>Time</th>
-                            <th>Description</th>
-                            <th>Done</th>
-                            <th>Options</th>
-                        </tr>
-                        <tr>
-                            <td>Arsti aeg</td>
-                            <td>30. oktoober 2018</td>
-                            <td>Silmaarsti aeg. Silmaarsti nimi on: dr Kukk</td>
-                            <td><Input type="checkbox" id="checkbox2" />{' '}</td>
-                            <td><Button>Muuda</Button><Button>Kustuta</Button></td>
-                        </tr>
-                        <tr>
-                            <td>Sõbraga kokkusaamine</td>
-                            <td>11. september kella 12 aeg</td>
-                            <td>Saan sõbraga kokku kohvikus hommikul</td>
-                            <td><Input type="checkbox" id="checkbox2" />{' '}</td>
-                            <td><Button>Muuda</Button><Button>Kustuta</Button></td>
-                        </tr>
-                    </table>
+                <div className="example">
+                    <h4>Custom animation</h4>
+                    <button className="btn btn-action" onClick={this.onOpenModal}>
+                        Open
+                    </button>
+                    {' '}
+                    <a
+                        href="https://github.com/pradel/react-responsive-modal/blob/master/docs/src/examples/custom-animation.js"
+                        target="_blank"
+                    >
+                        See source code
+                    </a>
+                    <Modal
+                        open={open}
+                        onClose={this.onCloseModal}
+                        center
+                        classNames={{
+                            transitionEnter: 'transition-enter',
+                            transitionEnterActive: 'transition-enter-active',
+                            transitionExit: 'transition-exit-active',
+                            transitionExitActive: 'transition-exit-active',
+                        }}
+                        animationDuration={1000}
+                    >
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                            pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+                            hendrerit risus, sed porttitor quam.
+                        </p>
+                    </Modal>
                 </div>
+                </body>
+
             </div>
-    );
+        );
     }
 }
+
 export default AppPage;
