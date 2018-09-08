1a59c3c1ab7a0eaf42dc459b2a9cbd2e39487edd
Triin <triin.lakspere@ttu.ee>
2018-09-08 10:21:58 +0300

    React fetch and form modifications

--- Vali-IT-projekt-frontend/src/App.js
+++ Vali-IT-projekt-frontend/src/App.js
@@ -3,30 +3,70 @@ import {Button, Form, FormGroup, Input, Col} from 'reactstrap';
 import logo from './images/logo.svg';
 import './css/App.css';
 import Modal from 'react-responsive-modal';
+import {userActions} from './actions/user.actions';
+
+const API = 'https://localhost:3000/login';
+const DEFAULT_QUERY = 'redux';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.onOpenModal = this.onOpenModal.bind(this);
-        this.onCloseModal = this.onCloseModal.bind(this);
+        this.onOpenModal1 = this.onOpenModal1.bind(this);
+        this.onOpenModal2 = this.onOpenModal2.bind(this);
+        this.onCloseModal1 = this.onCloseModal1.bind(this);
+        this.onCloseModal2 = this.onCloseModal2.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
         this.state = {
-            open: false
+            openFirstModal: false,
+            openSecondModal: false,
+            data:null,
+            username: '',
+            password: '',
+            submitted: false
         };
     }
 
-    onOpenModal = () => {
-        this.setState({ open: true });
-    };
+    componentDidMount() {
+        fetch(API + DEFAULT_QUERY)
+            .then(response => response.json())
+            .then(data => this.setState({ hits: data.hits }));
+    }
 
-    onCloseModal = () => {
-        this.setState({ open: false });
+    onOpenModal1 = () => {
+        this.setState({ openFirstModal: true });
     };
+    onOpenModal2 = () => {
+        this.setState({ openSecondModal: true });
+    };
+    onCloseModal1 = () => {
+        this.setState({ openFirstModal: false });
+    };
+    onCloseModal2 = () => {
+        this.setState({ openSecondModal: false });
+    };
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({submitted: true});
+        const {username, password} = this.state;
+        const {dispatch} = this.props;
+        if (username && password) {
+            dispatch(userActions.login(username, password));
+        }
+    }
 
   render() {
-      const {open} = this.state;
+      const {openFirstModal, openSecondModal} = this.state;
+      const { loggingIn } = this.props;
+      const { username, password, submitted } = this.state;
     return (
       <div className="App">
           <div className="limiter">
@@ -34,20 +74,26 @@ class App extends Component {
                   <img src={logo} className="App-logo" alt="logo" />
                   <h1 className="App-title">Kohtumised käeulatuses!</h1>
               <div className="container-login100">
-                  <Form>
+                  <Form onSubmit={this.handleSubmit}>
                       <FormGroup row>
                           <Col sm={11}>
-                            <Input type="text" name="username" id="username" placeholder="   Kasutajanimi" />
+                            <Input type="text" name="username" id="username" placeholder="   Kasutajanimi" value={username} onChange={this.handleChange}/>
+                              {submitted && !username &&
+                              <div className="help-block">Username is required!</div>
+                              }
                           </Col>
                       </FormGroup>
                       <FormGroup row>
                           <Col sm={11}>
-                          <Input type="password" name="password" id="password" placeholder="   Parool" />
+                          <Input type="password" name="password" id="password" placeholder="   Parool" value={password} onChange={this.handleChange}/>
+                              {submitted && !password &&
+                              <div className="help-block">Password is required!</div>
+                              }
                           </Col>
                       </FormGroup>
                       <Button className="login-btn">LOGI SISSE</Button>
-                      <Button className="new-user-button" onClick={this.onOpenModal}>LOO KASUTAJA</Button>
-                      <Modal open={open} onClose={this.onCloseModal} centered classNames={{ overlay: 'custom-overlay', modal: 'custom-modal'}}>
+                      <Button className="new-user-button" onClick={this.onOpenModal1}>LOO KASUTAJA</Button>
+                      <Modal open={openFirstModal} onClose={this.onCloseModal1} centered classNames={{ overlay: 'custom-overlay', modal: 'custom-modal'}}>
                           <h4>Uue kasutaja loomine</h4>
                           <Form>
                               <FormGroup>
@@ -64,11 +110,11 @@ class App extends Component {
                               </FormGroup>
                           </Form>
                           <Button color="warning">LOO</Button>{' '}
-                          <Button color="secondary" onClick={this.onCloseModal}>TÜHISTA</Button>{' '}
+                          <Button color="secondary" onClick={this.onCloseModal1}>TÜHISTA</Button>{' '}
                       </Modal>
                           <div className="text-right p-t-13 p-b-23">
-                              <p className="txt1">Unustasid <a href='#' className="txt2" onClick={this.onOpenModal}>kasutajanime/parooli?</a></p>
-                              <Modal open={open} onClose={this.onCloseModal} centered classNames={{ overlay: 'custom-overlay', modal: 'custom-modal' }}>
+                              <p className="txt1">Unustasid <a href='#' className="txt2" onClick={this.onOpenModal2}>kasutajanime/parooli?</a></p>
+                              <Modal open={openSecondModal} onClose={this.onCloseModal2} centered classNames={{ overlay: 'custom-overlay', modal: 'custom-modal' }}>
                                   <h4>Parooli taastamine</h4>
                                   <Form>
                                       <FormGroup>
@@ -76,7 +122,7 @@ class App extends Component {
                                       </FormGroup>
                                   </Form>
                                   <Button color="warning">SAADA</Button>{' '}
-                                  <Button color="secondary" onClick={this.onCloseModal}>TÜHISTA</Button>{' '}
+                                  <Button color="secondary" onClick={this.onCloseModal2}>TÜHISTA</Button>{' '}
                               </Modal>
                           </div>
                   </Form>
